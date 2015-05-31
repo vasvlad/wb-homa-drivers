0703c9d0f780bde826c959a7d273723ae49c381c
Uladzislau Vasilyeu <vasvlad@gmail.com>
2015-05-31 20:10:59 +0300

    All almost done

--- wb-homa-w1/sysfs_w1.cpp
+++ wb-homa-w1/sysfs_w1.cpp
@@ -81,22 +81,33 @@ TMaybe<char> TSysfsOnewireDevice::ReadOutput(int channel_number) const
 
     unsigned char c = 0;
     unsigned char result = 0;
+    int attempt = 3;
+    unsigned char array[3] = {255, 255, 255}; 
     std::ifstream file;
 //    std::string fileName=DeviceDir +"/state";
     std::string fileName=DeviceDir +"/output";
-    file.open(fileName.c_str(), std::ifstream::binary);
-    if (file.is_open()) {
-	c = file.get();
-        if (file.good() && c != 255) {
-            if ((c & (1<<channel_number))>0)
-                result = 1;
-            else
-                result = 0;
-            flag = true;
+    while (attempt >=0){
+        for (int i = 0; i<3; i++){
+            file.open(fileName.c_str(), std::ifstream::binary);
+            if (file.is_open()) {
+        	c = file.get();
+                if (file.good()) {
+                    if ((c & (1<<channel_number))>0)
+                        array[i] = 1;
+                    else
+                        array[i] = 0;
+                    flag = true;
+                }
+                file.close();
+            }
         }
-        file.close();
+        if (array[0] == array[1] && array[1] == array[2]){
+           result = array[0];
+           break;
+        }
+        cout << "Read output attempt"  << endl; 
+        attempt--;
     }
-
     if (flag)
         return (char)result;
     else
@@ -109,21 +120,36 @@ TMaybe<char> TSysfsOnewireDevice::ReadState(int channel_number) const
 
     unsigned char c = 0;
     unsigned char result = 0;
+    unsigned char array[3] = {255, 255, 255}; 
+    int attempt = 3;
     std::ifstream file;
     std::string fileName=DeviceDir +"/state";
-    file.open(fileName.c_str(), std::ifstream::binary);
-    if (file.is_open()) {
-	c = file.get();
-        if (file.good() && c != 255) {
-            if ((c & (1<<channel_number))>0)
-                result = 1;
-            else
-                result = 0;
-            flag = true;
-        }
-        file.close();
-    }
 
+    while (attempt >=0){
+        for (int i = 0; i<3; i++){
+            file.open(fileName.c_str(), std::ifstream::binary);
+            if (file.is_open()) {
+        	    c = file.get();
+                if (file.good()) {
+                    if ((c & (1<<channel_number))>0)
+                        array[i] = 1;
+                    else
+                        array[i] = 0;
+                    flag = true;
+                }
+                file.close();
+            }else{
+        	cout << "File state is not openned"  << endl; 
+            }
+        }
+        if (array[0] == array[1] && array[1] == array[2]){
+           result = array[0];
+           break;
+        }
+    
+        cout << "Read state attempt"  << endl; 
+        attempt--;
+    }
     if (flag)
         return (char)result;
     else
@@ -136,16 +162,36 @@ void TSysfsOnewireDevice::WriteOutput(int channel_number, int value)
     unsigned char c1 = 0;
     unsigned char c2 = 0;
     int attempt = 3;
+    int read_attempt = 5;
+    bool read_success = false;
     while (attempt >=0){
         std::fstream iofile;
         std::string fileName=DeviceDir +"/output";
-        iofile.open(fileName.c_str(), std::ios_base::in|std::ios_base::binary);
-        if (iofile.is_open()) {
-    	    c = iofile.get();
-            c1 = c;
-        }else
-            return;
-        iofile.close();
+
+        while (read_attempt >=0){
+            unsigned char array[3] = {255, 255, 255}; 
+            for (int i = 0; i<3; i++){
+                iofile.open(fileName.c_str(), std::ios_base::in|std::ios_base::binary);
+                if (iofile.is_open()) {
+            	    array[i] = iofile.get();
+                    iofile.close();
+                }else{
+                    cout << "File output is not openned"  << endl; 
+                }
+            }
+            if (array[0] == array[1] && array[1] == array[2]){
+               c = array[0];
+               c1 = c;
+               read_success = true;
+               break;
+            }
+            cout << "Read output attempt in Write Output"  << endl; 
+            read_attempt--;
+        }
+        if (!read_success){
+            cout << "File output is not correct"  << endl; 
+            break;
+        }
         iofile.open(fileName.c_str(), std::ios_base::out|std::ios_base::binary);
         cout << "C before = " << (bitset<8>) c << endl; 
         if (value == 0)
@@ -153,19 +199,18 @@ void TSysfsOnewireDevice::WriteOutput(int channel_number, int value)
         else
             c |= (1<<channel_number);
     
-        cout << "C after = " << (bitset<8>) c << endl; 
+        cout << "C after  = " << (bitset<8>) c << endl; 
         iofile.put(c);
         iofile.flush();
         iofile.close();
         iofile.open(fileName.c_str(), std::ios_base::in|std::ios_base::binary);
         if (iofile.is_open()) {
     	    c2 = iofile.get();
-        }else
-            return;
-        iofile.close();
-        if (c1 != c2)
-            return;
-        cout << "attempt"  << endl; 
+            iofile.close();
+            if (c1 != c2)
+                return;
+        }
+        cout << "attempt in WriteOutput"  << endl; 
 
         attempt--;
     }
@@ -173,13 +218,11 @@ void TSysfsOnewireDevice::WriteOutput(int channel_number, int value)
 }
 void TSysfsOnewireDevice::SwitchLight(int output_bit_number, int state_bit_number, int on)
 {
-    unsigned char c = 0;
     unsigned char state_bit = 0;
     unsigned char output_bit = 0;
+    int attempt = 3;
 
-    std::string fileName;
     printf("TSysfsOnewireDevice::SwitchLight\n");
-    std::fstream iofile;
     auto result = ReadState(state_bit_number);
     if (result.Defined()) {
         state_bit = *result;
@@ -188,61 +231,53 @@ void TSysfsOnewireDevice::SwitchLight(int output_bit_number, int state_bit_numbe
     }
 
     printf("State bit %i\n", (bool)state_bit);
-    iofile.close();
 
     if (on == state_bit)
         return;
 
-    fileName=DeviceDir +"/output";
-    iofile.open(fileName.c_str(), std::ios_base::in|std::ios_base::binary);
-    if (iofile.is_open()) {
-	c = iofile.get();
-    }else
-        return;
-    if ((c & (1<<output_bit_number))>0)
-        output_bit = 1;
-    else
-        output_bit = 0;
+    while (attempt >=0){
 
-    iofile.close();
-
-//    iofile.open(fileName.c_str(), std::ios_base::out|std::ios_base::binary);
-    printf("C1 before %i\n", c);
-    cout << "C1 before = " << (bitset<8>) c << endl; 
-
-    printf("Output bit %i\n", (bool)output_bit);
-    if (on == 1){
-        if (output_bit == 1) {
-            WriteOutput(output_bit_number, 0); 
-
-    	    printf("11111\n");
+        result = ReadOutput(output_bit_number);
+        if (result.Defined()) {
+            output_bit = *result;
         }else{
-            WriteOutput(output_bit_number, 0); 
-            WriteOutput(output_bit_number, 1); 
-
-    	    printf("22222\n");
+            return;
         }
-    }else{
-        if (output_bit == 1) {
-            WriteOutput(output_bit_number, 0); 
-    	    printf("33333\n");
+    
+        printf("Output bit %i\n", (bool)output_bit);
+        if (on == 1){
+            if (output_bit == 1) {
+                WriteOutput(output_bit_number, 0); 
+        	    printf("11111\n");
+            }else{
+                WriteOutput(output_bit_number, 0); 
+                WriteOutput(output_bit_number, 1); 
+        	    printf("22222\n");
+            }
         }else{
-            WriteOutput(output_bit_number, 1); 
-            WriteOutput(output_bit_number, 0); 
-            WriteOutput(output_bit_number, 1); 
-    	    printf("4444\n");
+            if (output_bit == 1) {
+                WriteOutput(output_bit_number, 0); 
+        	    printf("33333\n");
+            }else{
+                WriteOutput(output_bit_number, 1); 
+                WriteOutput(output_bit_number, 0); 
+                WriteOutput(output_bit_number, 1); 
+        	    printf("4444\n");
+            }
         }
+    
+        result = ReadState(state_bit_number);
+        if (result.Defined()) {
+            state_bit = *result;
+        }else{
+            return;
+        }
+        if (state_bit !=  on){
+            cout << "attempt in SwitchLight"  << endl; 
+            attempt--;
+        }else
+            break;
     }
-    iofile.close();
-
-    result = ReadState(state_bit_number);
-    if (result.Defined()) {
-        state_bit = *result;
-    }else{
-        return;
-    }
-
-
     printf("State bit %i\n", (bool)state_bit);
 
 }
