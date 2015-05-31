39f29e167355f3720dc214c63144c12bbd06cf2e
Uladzislau Vasilyeu <vasvlad@gmail.com>
2015-05-31 22:30:47 +0300

    Added channel 3

--- wb-homa-w1/main.cpp
+++ wb-homa-w1/main.cpp
@@ -156,7 +156,7 @@ void TMQTTOnewireHandler::RescanBus()
 void TMQTTOnewireHandler::OnMessage(const struct mosquitto_message *message)
 {
     int light_output[4] = {5,4,3,2};
-    int light_state[4] = {7,0,6,4};
+    int light_state[4] = {7,0,6,1};
     printf("TMQTTOnewireHandler::OnMessage. %s\n", message->topic);
     string topic = message->topic;
     string controls_prefix = string("/devices/") + MQTTConfig.Id + "/controls/";
